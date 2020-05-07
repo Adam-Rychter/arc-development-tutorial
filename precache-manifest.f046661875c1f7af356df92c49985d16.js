ae44ec019dd33342a92aaee2420762926c7e90d8
Adam Rychter <rychter.adam@gmail.com>
2020-05-07 18:05:09 +0200

    Updates

---
+++ precache-manifest.f046661875c1f7af356df92c49985d16.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b265d475056c9bea1d1d657c2071b027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9eceed4c5aeafcd8c848",
+    "url": "/static/js/2.a38bbaf5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.a38bbaf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fcf779a555f5180412d",
+    "url": "/static/js/main.70978a54.chunk.js"
+  },
+  {
+    "revision": "cd28dd722096fba81cfc",
+    "url": "/static/js/runtime-main.fe4c9649.js"
+  }
+]);
