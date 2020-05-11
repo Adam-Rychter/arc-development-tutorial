1b10f67c207e60fdc6b8eaef1cc21f3e08d01325
Adam Rychter <rychter.adam@gmail.com>
2020-05-11 23:31:19 +0200

    Almost finished menu

--- src/components/App.js
+++ src/components/App.js
@@ -11,14 +11,14 @@ function App() {
         <Header />
         <Switch>
           <Route exact path="/" component={() => <div>Home</div>}/>
-          <Route exact path="/services" component={() => <div>services</div>}/>
-          <Route exact path="/customsoftware" component={() => <div>customsoftware</div>}/>
-          <Route exact path="/mobileapps" component={() => <div>mobileapps</div>}/>
-          <Route exact path="/websites" component={() => <div>websites</div>}/>
-          <Route exact path="/revolution" component={() => <div>revolution</div>}/>
-          <Route exact path="/about" component={() => <div>about</div>}/>
-          <Route exact path="/contact" component={() => <div>contact</div>}/>
-          <Route exact path="/estimate" component={() => <div>estimate</div>}/>
+          <Route exact path="/services" component={() => <div>Services</div>}/>
+          <Route exact path="/customsoftware" component={() => <div>Custom Software</div>}/>
+          <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>}/>
+          <Route exact path="/websites" component={() => <div>Websites</div>}/>
+          <Route exact path="/revolution" component={() => <div>Revolution</div>}/>
+          <Route exact path="/about" component={() => <div>About</div>}/>
+          <Route exact path="/contact" component={() => <div>Contact</div>}/>
+          <Route exact path="/estimate" component={() => <div>Estimate</div>}/>
         </Switch>
       </BrowserRouter>
     </ThemeProvider>
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -7,6 +7,8 @@ import { makeStyles } from '@material-ui/styles'
 import Tabs from '@material-ui/core/Tabs'
 import Tab from '@material-ui/core/Tab'
 import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
 
 import logo from '../../assets/logo.svg'
 
@@ -34,7 +36,7 @@ const useStyles = makeStyles(theme => ({
     },
     logoContainer: {
         padding: 0,
-        "&:hover":{
+        "&:hover": {
             backgroundColor: "transparent"
         }
     },
@@ -53,17 +55,54 @@ const useStyles = makeStyles(theme => ({
         marginRight: "25px",
         height: "45px"
     },
-    
+    menu: {
+        backgroundColor: theme.palette.common.blue,
+        color: "white",
+        borderRadius: "0px"
+    },
+    menuItem: {
+        ...theme.typography.tab,
+        opacity: 0.7,
+        "&:hover": {
+            opacity: 1
+        }
+    }
+
 }))
 
 export default function Header(props) {
 
     const classes = useStyles()
     const [selectedTab, setSelectedTab] = useState(0)
+    const [anchorEl, setAnchorEl] = useState(null)
+    const [open, setOpen] = useState(false)
+    const [selectedIndex, setSelectedIndex] = useState(0)
 
     const handleTabSelection = (event, tabIndex) => {
         setSelectedTab(tabIndex)
     }
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget)
+        setOpen(true)
+    }
+
+    const handleMenuItemClick = (event, index) => {
+        setAnchorEl(null)
+        setOpen(false)
+        setSelectedIndex(index)
+    }
+
+    const handleClose = (event) => {
+        setAnchorEl(null)
+        setOpen(false)
+    }
+
+    const menuOptions = [
+        { name: "Services", link: "/services" },
+        { name: "Custom Software Development", link: "/customsoftware" },
+        { name: "Mobile App Development", link: "/mobileapps" },
+        { name: "Website Development", link: "/websites" }]
 
     useEffect(() => {
         if (window.location.pathname === "/" && selectedTab !== 0) {
@@ -88,19 +127,74 @@ export default function Header(props) {
             <ElevationScroll>
                 <AppBar position="fixed" >
                     <Toolbar disableGutters>
-                        <Button disableRipple component={Link} to="/" className={classes.logoContainer} onClick={() => setSelectedTab(0)}>
+                        <Button
+                            disableRipple
+                            component={Link}
+                            to="/"
+                            className={classes.logoContainer}
+                            onClick={() => setSelectedTab(0)} >
                             <img alt='company logo' src={logo} className={classes.logo} />
                         </Button>
-                        <Tabs value={selectedTab} className={classes.tabContainer} onChange={handleTabSelection} indicatorColor="primary">
-                            <Tab className={classes.tab} component={Link} to="/" label="Home" />
-                            <Tab className={classes.tab} component={Link} to="/services" label="Services" />
-                            <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution" />
-                            <Tab className={classes.tab} component={Link} to="/about" label="About Us" />
-                            <Tab className={classes.tab} component={Link} to="/contact" label="Contact Us" />
+                        <Tabs
+                            value={selectedTab}
+                            className={classes.tabContainer}
+                            onChange={handleTabSelection}
+                            indicatorColor="primary" >
+                            <Tab
+                                className={classes.tab}
+                                component={Link}
+                                to="/"
+                                label="Home" />
+                            <Tab
+                                aria-owns={anchorEl ? "simple-menu" : undefined}
+                                aria-haspopup={anchorEl ? "true" : undefined}
+                                className={classes.tab}
+                                component={Link}
+                                onMouseOver={event => handleClick(event)}
+                                to="/services"
+                                label="Services" />
+                            <Tab
+                                className={classes.tab}
+                                component={Link}
+                                to="/revolution"
+                                label="The Revolution" />
+                            <Tab
+                                className={classes.tab}
+                                component={Link}
+                                to="/about"
+                                label="About Us" />
+                            <Tab
+                                className={classes.tab}
+                                component={Link}
+                                to="/contact"
+                                label="Contact Us" />
                         </Tabs>
-                        <Button variant="contained" color="secondary" className={classes.button}>
+                        <Button
+                            variant="contained"
+                            color="secondary"
+                            className={classes.button}>
                             Free Estimate
                         </Button>
+                        <Menu
+                            id="simple-menu"
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            classes={{ paper: classes.menu }}
+                            MenuListProps={{ onMouseLeave: handleClose }}
+                            elevation={0}>
+
+                            {menuOptions.map((option, index) => (
+                                <MenuItem key={option} component={Link}
+                                to={option.link}
+                                classes={{root: classes.menuItem}}
+                                onClick={(event) => {handleMenuItemClick(event, index); setSelectedTab(1)}}
+                                selected={index === selectedIndex && selectedTab === 1}>
+                                    {option.name}
+                                </MenuItem>
+                            ))}
+
+                        </Menu>
                     </Toolbar>
                 </AppBar>
             </ElevationScroll>
